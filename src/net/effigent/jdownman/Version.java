a6328c0de85dcd8cbb0ca73afd4f4f58100a8a76
ViPandey <ViPandey@2464b46d-2437-0410-a502-d9d66e5afa2f>
2007-08-18 00:32:10 +0000

    first checkin

---
+++ src/net/effigent/jdownman/Version.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package net.effigent.jdownman;
+
+/**
+ * @author vipul
+ *
+ */
+public class Version {
+	
+	private static final String version =  "A01";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(" Version : "+version);
+	}
+
+	/**
+	 * @return the version
+	 */
+	public static String getVersion() {
+		return version;
+	}
+
+}
