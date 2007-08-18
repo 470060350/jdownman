a6b034022279c8d470f4195cfbceeac5de8f698e
ViPandey <ViPandey@2464b46d-2437-0410-a502-d9d66e5afa2f>
2007-08-18 00:31:47 +0000

    added new methods

--- trunk/src/net/effigent/jdownman/DownloadManager.java
+++ trunk/src/net/effigent/jdownman/DownloadManager.java
@@ -14,12 +14,43 @@ import java.net.URL;
  */
 public interface DownloadManager {
 	
+
 	/**
 	 * 
+	 * @param destinationFile
+	 * @param urls
+	 */
+	public void downloadFile(File destinationFile,URL[] urls) throws DownloadException;
+
+	/**
+	 * 
+	 * @param destinationFile
 	 * @param urls
 	 * @param listener
+	 * @throws DownloadException
 	 */
-	public void downloadFile(File destinationFile,URL[] urls,DownloadListener listener, Object CRC);
+	public void downloadFile(File destinationFile,URL[] urls,DownloadListener listener) throws DownloadException;
+
+	/**
+	 * 
+	 * @param destinationFile
+	 * @param urls
+	 * @param CRC
+	 * @param length
+	 * @throws DownloadException
+	 */
+	public void downloadFile(File destinationFile,URL[] urls,Object CRC, long length) throws DownloadException;
+
+	/**
+	 * 
+	 * @param destinationFile
+	 * @param urls
+	 * @param listener
+	 * @param CRC
+	 * @param length
+	 * @throws DownloadException
+	 */
+	public void downloadFile(File destinationFile,URL[] urls,DownloadListener listener, Object CRC, long length) throws DownloadException;
 
 
 }
